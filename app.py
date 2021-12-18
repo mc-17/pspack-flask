9deaa850b87689817a79d212ffc2251567d4b724
Michael Cowell <michael.cowell@tutanota.com>
2021-12-18 23:25:34 +0000

    Many changes
    - Retry 3 times when sending payload
    - CSS changes
    - Code tidyup

--- app.py
+++ app.py
@@ -6,29 +6,51 @@ from sender import send
 app = Flask(__name__)
 
 
+def check_payload_path(path: str):
+    path = os.path.join("payload", os.path.basename(path))
+    return os.path.isfile(path)
+
+
 @app.route('/')
 def index():
     ua = request.headers.get("User-Agent", None)
+    ua_part = ua
     try:
         if ua:
             ua_part = ua[ua.index("PlayStation 4/") + len("PlayStation 4/"):]
             ua_part = ua_part[:ua_part.index(")")]
     except ValueError:
         print("Not PS4")
-        ua_part = ua
 
-    return render_template("index.html", version=ua_part)
+    return render_template("index.html", version=ua_part, payloads=sorted(os.listdir("payload"),
+                                                                          key=lambda name: "gold" not in name))
+
+
+queue = {}
+
+
+@app.route("/expect/<payload>")
+def expect(payload):
+    payload = unquote_plus(payload)
+    if check_payload_path(f"payload/{payload}"):
+        print("Queuing payload request")
+        queue[request.remote_addr] = os.path.join("payload", payload)
+    else:
+        print("Bad payload request!")
+    return ""
 
 
 @app.route("/log/<msg>")
 def log(msg):
     msg = unquote_plus(msg)
-    if "done" in msg or "already" in msg:
-        # success message, send HEN
-        print(f"Sending golden hen to {request.remote_addr}")
-        send(request.remote_addr, 9020, "payload/goldhen_2.0b_900.bin")
-
     print(msg)
+
+    if "done" in msg:
+        payload = queue.get(request.remote_addr, "payload/goldhen_2.0b_900.bin")
+        print(f"Sending {payload} to {request.remote_addr}...")
+        send(request.remote_addr, 9020, payload)
+        print(f"Done!")
+
     return "OK"
 
 
--- sender.py
+++ sender.py
@@ -1,13 +1,20 @@
 import socket
+import time
 
-def send(ip, port, file):
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
-    clientSocket.settimeout(3000)
-    clientSocket.connect((ip, port))
 
-    try:
-        with open(file, "rb") as fp:
-            clientSocket.sendfile(fp)
-            clientSocket.close()
-    finally:
-        clientSocket.close()
+def send(ip: str, port: int, file: str) -> None:
+    for _ in range(3):
+        try:
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
+            client.settimeout(3000)
+            client.connect((ip, port))
+
+            try:
+                with open(file, "rb") as fp:
+                    client.sendfile(fp)
+            finally:
+                client.close()
+        except (ConnectionRefusedError, OSError):
+            # Sometimes the socket isn't bound :(
+            time.sleep(1)
+
