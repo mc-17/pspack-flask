b852c64a32d966f264ff8451eb4d4fcb0da41a30
Michael Cowell <michael.cowell@tutanota.com>
2021-12-26 17:01:00 +0000

    Update sender.py

--- sender.py
+++ sender.py
@@ -1,13 +1,13 @@
 import socket
 
+
 def send(ip, port, file):
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
-    clientSocket.settimeout(3000)
-    clientSocket.connect((ip, port))
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
+    client_socket.settimeout(3000)
+    client_socket.connect((ip, port))
 
     try:
         with open(file, "rb") as fp:
-            clientSocket.sendfile(fp)
-            clientSocket.close()
+            client_socket.sendfile(fp)
     finally:
-        clientSocket.close()
+        client_socket.close()
