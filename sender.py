6e9e45b63a536953e9f97c8014f781dd03fe41fc
Michael Cowell <michael.cowell@tutanota.com>
2022-01-09 20:39:15 +0000

    Attempt to send payload three times

--- sender.py
+++ sender.py
@@ -1,13 +1,19 @@
+import time
 import socket
 
 
 def send(ip, port, file):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
-    client_socket.settimeout(3000)
-    client_socket.connect((ip, port))
+    for i in range(3):
+        print(f"Sending payload: attempt {i}...")
+        time.sleep(1)
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
+        client_socket.settimeout(3000)
+        client_socket.connect((ip, port))
 
-    try:
-        with open(file, "rb") as fp:
-            client_socket.sendfile(fp)
-    finally:
-        client_socket.close()
+        try:
+            with open(file, "rb") as fp:
+                client_socket.sendfile(fp)
+            print(f"Sending payload: attempt {i} succeeded!")
+            break
+        finally:
+            client_socket.close()
